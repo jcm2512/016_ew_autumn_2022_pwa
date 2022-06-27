d271c574a3c094fbe4849063919e105de28ccffb
jcm2512 <joe.magalotti@gmail.com>
2022-06-28 02:45:50 +0900

    return cache

--- public/service-worker.js
+++ public/service-worker.js
@@ -50,9 +50,10 @@ self.addEventListener("fetch", (evt) => {
   }
   evt.respondWith(
     fetch(evt.request).catch(() => {
-      return caches.open(CACHE_NAME).then((cache) => {
-        return cache.match("offline.html");
-      });
+      return caches.open(CACHE_NAME);
+      //   .then((cache) => {
+      //     return cache.match("offline.html");
+      //   });
     })
   );
 });
