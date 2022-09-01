a3014d9c9105c764df8884f45ad41fb0c44d712d
jcm2512 <joe.magalotti@gmail.com>
2022-09-02 00:35:21 +0900

    trivia update

--- src/pages/Trivia.svelte
+++ src/pages/Trivia.svelte
@@ -1,24 +1,37 @@
+<script>
+  import Fact_1_en from "./Fact_1_en.svelte";
+  import Fact_1_jp from "./Fact_1_jp.svelte";
+
+  let eng = true;
+</script>
+
 <div id="content" class="grid-top grid">
   <div id="menu_heading" class="menu_heading">
     <div class="main">MONSTER</div>
     <div class="sub">STAMP RALLY</div>
   </div>
-  <div class="message">
-    <div class="title jp-font bold">Did you know?</div>
+  <div id="menu">
+    <!-- <div class="title jp-font bold">知っていましたか?</div> -->
+    <div on:click={() => (eng = !eng)}>
+      {#if eng}
+        <Fact_1_en />
+      {:else}
+        <Fact_1_jp />{/if}
+    </div>
+    <div class="bottom_message jp-font">
+      <p>次回の更新は次の金曜日です。</p>
+      <p>またお会いしましょう！</p>
+    </div>
   </div>
 </div>
 
 <style>
+  .bottom_message {
+    margin-top: 6vw;
+    font-size: 6vw;
+  }
+
   #menu_heading .sub {
     margin-top: -5vw;
-  }
-
-  .message {
-    background-color: var(--blue);
-    font-size: 10vw;
-    border-radius: 5vw;
-  }
-
-  .title {
   }
 </style>
