f819b6b79bc38c16bd4b4ff2ed88edc7aba35f3d
jcm2512 <joe.magalotti@gmail.com>
2022-09-16 01:37:27 +0900

    animated stamps

--- src/pages/Teachers.svelte
+++ src/pages/Teachers.svelte
@@ -6,9 +6,11 @@
     updateStamps,
     viewAllStamps,
   } from "../store.js";
+  import { animateCSS } from "../animateCSS.svelte";
 
-  let stageCards, next, heading, menu;
-  let CARDS = [];
+  // let stageCards, next, heading, menu;
+  // let CARDS = [];
+  let DOMelements = [];
 
   // export let id;
 </script>
@@ -19,16 +21,20 @@
   </div>
   <div class="menu_item stamps">
     {#key $updateStamps}
-      {#each Object.keys($stampCollection[$menuState].stamps[area_name].area_stamps) as stamp}
+      {#each Object.keys($stampCollection[$menuState].stamps[area_name].area_stamps) as stamp, index}
         {#if $stampCollection[$menuState].stamps[area_name].area_stamps[stamp].found || $viewAllStamps}
           <img
+            bind:this={DOMelements[index]}
+            on:click|preventDefault={() => {
+              animateCSS(DOMelements[index], "rubberBand");
+            }}
             src={$stampCollection[$menuState].stamps[area_name].area_stamps[
               stamp
             ].img}
             alt={$stampCollection[$menuState].stamps[area_name].area_stamps[
               stamp
             ].name}
-            class="stamp"
+            class="stamp animate__faster"
           />
         {:else}
           <img
