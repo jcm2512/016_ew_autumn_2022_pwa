4a36b87292abce298776fbdbca02f6effcb80edf
jcm2512 <joe.magalotti@gmail.com>
2022-10-02 15:25:28 +0900

    exclude html from sw

--- workbox-config.js
+++ workbox-config.js
@@ -1,6 +1,6 @@
 module.exports = {
   globDirectory: "public/",
-  globPatterns: ["**/*.{png,svg,json,css,js,ttf,html}"],
+  globPatterns: ["**/*.{png,svg,json,css,js,ttf}"],
   swDest: "public/sw.js",
   maximumFileSizeToCacheInBytes: "5000000",
 };
