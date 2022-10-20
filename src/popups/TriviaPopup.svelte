357de3f77867891a5d2e9f7ab00ae76d44b412f5
jcm2512 <joe.magalotti@gmail.com>
2022-10-20 22:57:11 +0900

    trial 3

--- src/popups/TriviaPopup.svelte
+++ src/popups/TriviaPopup.svelte
@@ -81,18 +81,18 @@
     console.log(option);
     // let parent = event.path[0];
     // let selected = parent.getAttribute("id");
-    // if (TRIVIA[num].answer == selected && !$foundStamp.found) {
-    //   // If correct
-    //   $foundStamp.found = true;
-    //   $foundStamp.count += 1;
-    //   $stampCount += 1;
-    //   // Show Stamp Button
-    //   $triviaCorrect = true;
-    //   // parent.classList.add("correct");
-    // } else {
-    //   // If incorrect
-    //   // parent.classList.add("incorrect");
-    // }
+    if (TRIVIA[num].answer == option && !$foundStamp.found) {
+      // If correct
+      $foundStamp.found = true;
+      $foundStamp.count += 1;
+      $stampCount += 1;
+      // Show Stamp Button
+      $triviaCorrect = true;
+      // parent.classList.add("correct");
+    } else {
+      // If incorrect
+      // parent.classList.add("incorrect");
+    }
   }
   function button() {
     $triggerMenuState += 1;
@@ -124,11 +124,7 @@
     <div id="popup_message">{TRIVIA[num].question}</div>
     <div id="trivia_options">
       {#each TRIVIA[num].options as option, index}
-        <div
-          class="choices"
-          id={index}
-          on:click={() => optionSelected(option[index])}
-        >
+        <div class="choices" id={index} on:click={() => optionSelected(index)}>
           {option}
         </div>
       {/each}
